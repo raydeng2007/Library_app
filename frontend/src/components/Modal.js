bbc71575424421ae1b34a314101653a9648eb582
Raymond <r6deng@uwaterloo.ca>
2020-06-18 13:34:38 -0400

    added comments and format code

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,78 +1,78 @@
 import React, { Component } from "react";
 import {
-    Button,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Form,
-    FormGroup,
-    Input,
-    Label,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label,
 } from "reactstrap";
 
 export default class CreateModal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activeItem: this.props.activeItem,
-        };
-    }
-
-    handleChange = (e) => {
-        let { name, value } = e.target;
-        if (e.target.type === "checkbox") {
-            value = e.target.checked;
-        }
-        const activeItem = { ...this.state.activeItem, [name]: value };
-        this.setState({ activeItem });
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: this.props.activeItem,
     };
+  }
 
-    render() {
-        const { toggle, onSave } = this.props;
-        return (
-            <Modal isOpen={true} toggle={toggle}>
-                <ModalHeader toggle={toggle}>Add a Book Item </ModalHeader>
-                <ModalBody>
-                    <Form>
-                        <FormGroup>
-                            <Label for="title">Title</Label>
-                            <Input
-                                type="text"
-                                name="title"
-                                value={this.state.activeItem.title}
-                                onChange={this.handleChange}
-                                placeholder="Enter Book Title"
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="author">Author</Label>
-                            <Input
-                                type="text"
-                                name="author"
-                                value={this.state.activeItem.author}
-                                onChange={this.handleChange}
-                                placeholder="Enter Book Author"
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="book_id">Book Id</Label>
-                            <Input
-                                type="text"
-                                name="book_id"
-                                value={this.state.activeItem.book_id}
-                                onChange={this.handleChange}
-                                placeholder="Enter Book Id"
-                            />
-                        </FormGroup>
-                    </Form>
-                </ModalBody>
-                <ModalFooter>
-                    <Button color="success" onClick={() => onSave(this.state.activeItem)}>
-                        Save
-                    </Button>
-                </ModalFooter>
-            </Modal>
-        );
+  handleChange = (e) => {
+    let { name, value } = e.target;
+    if (e.target.type === "checkbox") {
+      value = e.target.checked;
     }
+    const activeItem = { ...this.state.activeItem, [name]: value };
+    this.setState({ activeItem });
+  };
+
+  render() {
+    const { toggle, onSave } = this.props;
+    return (
+      <Modal isOpen={true} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Add a Book Item </ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="title">Title</Label>
+              <Input
+                type="text"
+                name="title"
+                value={this.state.activeItem.title}
+                onChange={this.handleChange}
+                placeholder="Enter Book Title"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="author">Author</Label>
+              <Input
+                type="text"
+                name="author"
+                value={this.state.activeItem.author}
+                onChange={this.handleChange}
+                placeholder="Enter Book Author"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="book_id">Book Id</Label>
+              <Input
+                type="text"
+                name="book_id"
+                value={this.state.activeItem.book_id}
+                onChange={this.handleChange}
+                placeholder="Enter Book Id"
+              />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="success" onClick={() => onSave(this.state.activeItem)}>
+            Save
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
 }
